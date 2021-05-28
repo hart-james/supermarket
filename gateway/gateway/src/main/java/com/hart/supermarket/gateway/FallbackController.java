9238f78a6d0af8ec6cf42047aa39f1f602c57a01
James Hart <hart87@gmail.com>
2021-05-28 13:02:41 -0400

    Hystrix Circuit Breaker & Dashboard

---
+++ gateway/gateway/src/main/java/com/hart/supermarket/gateway/FallbackController.java
@@ -0,0 +1,14 @@
+package com.hart.supermarket.gateway;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/itemsFallback")
+    public Mono<String> itemsServiceFallback(){
+        return Mono.just("Items Service is taking too long to respond or is down");
+    }
+}
