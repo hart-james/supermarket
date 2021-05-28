19bf7f39d51f7810a2850d56e8170dd043b99e87
James Hart <hart87@gmail.com>
2021-05-28 12:14:12 -0400

    API Gateway

---
+++ gateway/gateway/src/main/java/com/hart/supermarket/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.hart.supermarket.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+		System.out.println("Gateway Service");
+	}
+
+}
