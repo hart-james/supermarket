e15953c4e4b70ed599bf145710af698197d1f958
James Hart <hart87@gmail.com>
2021-05-31 12:23:12 -0400

    Transaction & Sales Service

---
+++ sales/src/main/java/com/hart/supermarket/sales/SalesApplication.java
@@ -0,0 +1,16 @@
+package com.hart.supermarket.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApplication.class, args);
+		System.out.println("Sales Service.");
+	}
+
+}
