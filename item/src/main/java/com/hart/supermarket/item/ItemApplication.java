f8a0574bfa0a1197cf642cece0db3d71117a9ff6
James Hart <hart87@gmail.com>
2021-06-01 10:48:10 -0400

    Token Bucket Rate Limiting On Endpoint

--- item/src/main/java/com/hart/supermarket/item/ItemApplication.java
+++ item/src/main/java/com/hart/supermarket/item/ItemApplication.java
@@ -1,10 +1,14 @@
 package com.hart.supermarket.item;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,4 +21,10 @@ public class ItemApplication {
 		System.out.println("Items Service.");
 	}
 
+	@Bean
+	RateLimiter rateLimiter(){
+		return RateLimiter.create(
+				2,
+				Duration.ofSeconds(10));
+	}
 }
--- item/src/main/java/com/hart/supermarket/item/api/ItemsRestController.java
+++ item/src/main/java/com/hart/supermarket/item/api/ItemsRestController.java
@@ -1,6 +1,8 @@
 package com.hart.supermarket.item.api;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.hart.supermarket.item.service.ItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,16 @@ public class ItemsRestController {
 
 //    @Value("${key.name}")
 //    String name;  //For Config Server Testing
+    @Autowired
+    private RateLimiter rateLimiter;
 
     @GetMapping(value = "/hello", produces = { "application/json" } )
     public String TestService() {
-        return ItemsService.GetACliffBarBrand();
+        boolean okToGo = rateLimiter.tryAcquire();
+        if (okToGo){
+            return ItemsService.GetACliffBarBrand();
+        }
+        return "Too many requests.";
     }
 
 }
