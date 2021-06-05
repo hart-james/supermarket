89e171d07dd9a3cb33167e02df01bc8ff5b744fd
James Hart <hart87@gmail.com>
2021-06-04 20:08:22 -0400

    Batch Service Added. And Release.

---
+++ batch/src/test/java/com/hart/supermarket/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hart.supermarket.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
