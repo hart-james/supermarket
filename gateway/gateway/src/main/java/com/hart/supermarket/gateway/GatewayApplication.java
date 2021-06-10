719b0cf709349f1771e07d783a11665934fbdaeb
James Hart <hart87@gmail.com>
2021-06-10 10:06:39 -0400

    Gateway Filtered Rate Limiting. Token Bucket.

--- gateway/gateway/src/main/java/com/hart/supermarket/gateway/GatewayApplication.java
+++ gateway/gateway/src/main/java/com/hart/supermarket/gateway/GatewayApplication.java
@@ -1,9 +1,13 @@
 package com.hart.supermarket.gateway;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,6 +17,13 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 		System.out.println("Gateway Service East");
+	}
+
+	@Bean
+	RateLimiter rateLimiter(){
+		return RateLimiter.create(
+				2,
+				Duration.ofSeconds(10));
 	}
 
 }
--- gateway/gateway/src/main/java/com/hart/supermarket/gateway/filter/RateFilter.java
+++ gateway/gateway/src/main/java/com/hart/supermarket/gateway/filter/RateFilter.java
@@ -1,10 +1,13 @@
 package com.hart.supermarket.gateway.filter;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,13 +15,27 @@ public class RateFilter extends AbstractGatewayFilterFactory {
 
     final Logger logger = LoggerFactory.getLogger(RateFilter.class);
 
+    @Autowired
+    private RateLimiter rateLimiter;
+
     @Override
     public GatewayFilter apply(Object config) {
+        logger.info("RATE LIMITING FILTER");
+
         return (exchange, chain) -> {
-            logger.info("RATE LIMITING FILTER");
-            return chain.filter(exchange);
+
+            boolean okToGo = rateLimiter.tryAcquire();
+            if (okToGo){
+                logger.info("RATE LIMITING FILTER");
+                return chain.filter(exchange);
+            }
+
+            // set TOO_MANY_REQUESTS 429 response and stop the processing
+            exchange.getResponse().setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
+            return exchange.getResponse().setComplete();
         };
     }
+
 
 
 }
